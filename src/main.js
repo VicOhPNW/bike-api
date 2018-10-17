ee9f51ee3967df028300c546868ee03da32aef51
Mel Yasuda and Victoria Oh <victoria@>
2018-10-17 14:24:51 -0700

    list title of bikes

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import './styles.css';
 import { StolenBikeList } from './bike-api.js';
 
 $(document).ready(function() {
-  let stolenBike = new StolenBikeList();  // create instance of WeatherService class
-  let promise = stolenBike.getStolenBikes();  // call the instance method and pass in user input
+  let stolenBike = new StolenBikeList();
+  let promise = stolenBike.getStolenBikes();
 
   promise.then(function(response) {
     let body = JSON.parse(response);
-    $('#stolen-list').append(`${body.bikes[0].title}`);
+
+      for(var i=0; i < body.bikes.length; i++) {
+        let title = body.bikes[i].title;
+        $(".list").append("<li>" + title +"</li>");
+      }
   }, function(error) {
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   });
